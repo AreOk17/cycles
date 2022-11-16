66264eff481675dddd05b534bc60d8b643f63b13
1 <Anasko162@Mail.ru>
2022-11-16 18:04:50 +0300

    Циклы. Часть 2. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int mounth = 0;
-        int total = 0;
-        int salary = 15000;
-        while (total < 2_459_000) {
-            total = total + salary;
-            mounth = mounth + 1;
-            System.out.println("Месяц " +mounth+ " сумма накоплений:" +total);
+        int i = 1;
+        while (i <=10) {
+            System.out.print(" " +i++ );
         }
-    }
+        System.out.println("");
+        for (int kol = 10; kol >= 1; kol --) {
+
+            System.out.print("  " +kol);
+
+}
+}
 }
