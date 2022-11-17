be7b16243b427d9ccc9015ab2326bbb8c71f63b9
1 <Anasko162@Mail.ru>
2022-11-17 18:44:19 +0300

    Циклы. Часть 2. Домашнее задание 2. Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int mounth = 0;
         int vkladvasilia  = 15000;
-int procentpovklady = (vkladvasilia / 1000) * 7;
+int procentpovklady = (vkladvasilia / 100) * 7;
         while (vkladvasilia < 12_000_000) {
             vkladvasilia = vkladvasilia + procentpovklady;
             mounth = mounth + 1;
