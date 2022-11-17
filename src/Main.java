2ec0d8bbc2749d76fe659ae37f951d0fe92cabdc
1 <Anasko162@Mail.ru>
2022-11-17 19:06:08 +0300

    Циклы. Часть 2. Домашнее задание 2. Задание 2.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ int procentpovklady = (vkladvasilia / 100) * 7;
         while (vkladvasilia < 12_000_000) {
             vkladvasilia = vkladvasilia + procentpovklady;
             mounth = mounth + 1;
+            if (mounth % 6 == 0) {
+                System.out.println("Месяц " +mounth + "cумма накоплений " + vkladvasilia);
+            }
 
-            System.out.println("Месяц " +mounth+ " сумма накоплений:" +vkladvasilia);
         }
     }
 }
