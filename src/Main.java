11b2ba36eb832fe71f71b9498f62e8dd5ecdd7e9
1 <Anasko162@Mail.ru>
2022-11-23 18:13:30 +0300

    Циклы. Часть 2. Домашнее задание 3. Задание 1. Не очень поняла как её правильно выполнить.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-       int vznos = 15000;
-       int procentpovklady = 7;
-       int mounth = 1;
-       int summavznosa = (vznos / 100) * procentpovklady;
-        System.out.println(summavznosa);
-        int summavklada = vznos + summavznosa;
-        System.out.println(summavklada);
-        while (mounth <= 108) {
-            vznos = vznos + summavznosa;
-            mounth = mounth +1;
-            if ( mounth % 6 == 0) {
-                System.out.println("месяц: " +mounth+ " Cумма накоплений: " +vznos);
-            }
-        }
-       }
+        int years = 200;
+        int vesokosniy = 79;
+       for (int i = 0; i <= years; i++) {
+           if (i % vesokosniy == 0) {
+           System.out.println(i+ "+79" );
+}
     }
+}
+}
